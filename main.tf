937a363efabf8b586a6ce104fecd09d21e724b6f
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-11-11 22:23:38 -0700

    fix bad ami ids

--- main.tf
+++ main.tf
@@ -1,12 +1,22 @@
+# AMI ID,AMI Name,Region
+# ami-09190d816c07cca00,al2023-ami-minimal-2023.6.20241010.0-kernel-6.1-x86_64,us-east-1
+# ami-0fb081d9d19c88d61,al2023-ami-minimal-2023.5.20240819.0-kernel-6.1-x86_64,us-east-2
+# ami-086a2d9f28fe39627,al2023-ami-minimal-2023.6.20241031.0-kernel-6.1-x86_64,us-west-1
+# ami-0c968d9bc191231c8,al2023-ami-minimal-2023.6.20241031.0-kernel-6.1-x86_64,us-west-2
+# ami-02e6e695469fdfaac,al2023-ami-minimal-2023.5.20240819.0-kernel-6.1-x86_64,eu-west-1
+# None,il-central-1
+
+
 variable "ami_ids" {
   default = {
-    "us-east-1" = "ami-06ff9e92a8fecbb77"
+    "us-east-1" = "ami-09190d816c07cca00"
     "us-east-2" = "ami-0fb081d9d19c88d61"
-    "us-west-1" = "ami-0d4c90c7f333a7d17"
-    "us-west-2" = "ami-03b2bc8987ca3dce7"
-    "eu-west-1" = "ami-0eae74a2795d511c8"
+    "us-west-1" = "ami-086a2d9f28fe39627"
+    "us-west-2" = "ami-0c968d9bc191231c8"
+    "eu-west-1" = "ami-02e6e695469fdfaac"
   }
 }
+
 
 module "vpc_ec2_us_east_1" {
   source         = "./module"
